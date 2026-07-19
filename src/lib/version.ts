6da82ff6b38406cc9b49c60df2cdabd9efd90b85
wizzare <52053255+wizzare@users.noreply.github.com>
2026-07-20 01:26:40 +0700

    chore(release): set version 4.4.1 build 78

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,5 +1,5 @@
-export const APP_VERSION = "4.4";
+export const APP_VERSION = "4.4.1";
 
-export const RELEASE_NAME = "BHUMI AMARTYA V4 BUILD 77";
+export const RELEASE_NAME = "BHUMI AMARTYA V4 BUILD 78";
 
-export const LAST_UPDATED = "2026-07-19";
+export const LAST_UPDATED = "2026-07-20";
