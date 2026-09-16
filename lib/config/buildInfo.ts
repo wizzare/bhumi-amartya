0d1330f905b2664224bd7044332aca1761c00a49
wizzare <52053255+wizzare@users.noreply.github.com>
2026-09-16 23:53:50 +0700

    chore(release): bump to Build 113 (5.0.13)

--- lib/config/buildInfo.ts
+++ lib/config/buildInfo.ts
@@ -1,7 +1,7 @@
 import { Capacitor } from "@capacitor/core";
 
-export const CURRENT_VERSION_NAME = "5.0.12";
-export const CURRENT_VERSION_CODE = 112;
+export const CURRENT_VERSION_NAME = "5.0.13";
+export const CURRENT_VERSION_CODE = 113;
 export const CURRENT_BUILD_NUMBER = "112";
 export const INTERNAL_TESTING_TRACK = "Internal Testing";
 
